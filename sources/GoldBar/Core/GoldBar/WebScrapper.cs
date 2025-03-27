a3b9a3f59cc5e2c4f920b9e0e45f08525502d466
Ehsan Haghpanah <esn.hagh@gmail.com>
2025-03-27 18:45:39 +0330

    importing .NET Framework artifacts

---
+++ sources/GoldBar/Core/GoldBar/WebScrapper.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Copyright (C) scenüs, 2025.
+ * All rights reserved.
+ * Ehsan Haghpanah; haghpanah@scenus.com
+ */
+
+using System.Runtime.InteropServices;
+
+namespace GoldBar
+{
+	/// <summary>
+	/// WebScrapper (Interface)
+	/// </summary>
+	[ComVisible(true)]
+	[InterfaceType(ComInterfaceType.InterfaceIsDual)]
+	[Guid("26950555-3231-4519-AF70-9CC870D1CF98")]
+	public interface IWebScrapper
+	{
+		double GetCurrentPrice();
+	}
+
+	/// <summary>
+	/// WebScrapper
+	/// </summary>
+	[ComVisible(true)]
+	[ClassInterface(ClassInterfaceType.None)]
+	[Guid("D490B988-B65F-4F10-BA0B-0770CACDBA35")]
+	[ProgId("GoldBarDll.WebScrapper")]
+	public sealed class WebScrapper : IWebScrapper
+	{
+		public double GetCurrentPrice()
+		{
+			return 1001;
+		}
+	}
+}
